aedb4f4eacd5dfd86d7448005ce8a848bbb972ab
William Jolivet <william.jolivet@epitech.eu>
2026-03-25 11:35:20 +0100

    feat: update creneaux filtering to include user participation in responses

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,9 +3,11 @@ import { listCrenaux } from '$lib/server/data';
 
 export const load: PageServerLoad = async ({ locals, url }) => {
 	const error = url.searchParams.get('error');
-	// Only show creneaux created by the logged-in user
+	// Show creneaux created by or participated in by the logged-in user
 	const crenaux = locals.user
-		? listCrenaux().filter((c) => c.created_by === locals.user!.email)
+		? listCrenaux().filter(
+				(c) => c.created_by === locals.user!.email || locals.user!.email in (c.responses ?? {})
+			)
 		: [];
 
 	return {
